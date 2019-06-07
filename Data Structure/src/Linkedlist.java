58babcacc74214a232b60371c92656adf3099e4f
sudeep <srajput@aisplstore.com>
2019-06-07 20:37:17 +0530

    Linked List

---
+++ Data Structure/src/Linkedlist.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author SUDEEP RAJPUT
+ *
+ */
+public class Linkedlist {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
