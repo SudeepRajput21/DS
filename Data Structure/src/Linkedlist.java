e5420fb51d38bfbd044f9344afb3b1d72480820e
sudeep <sudeeprajput21@gmail.com>
2019-06-07 22:21:23 +0530

    Creating a LinkedList

--- Data Structure/src/Linkedlist.java
+++ Data Structure/src/Linkedlist.java
@@ -1,19 +1,25 @@
-/**
- * 
- */
 
-/**
- * @author SUDEEP RAJPUT
- *
- */
 public class Linkedlist {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Node head;           //head of node
+	
+	static class Node{   //LL Node. This inner class is made static so that main()can access it.
+	int data;
+	Node next;
+	Node(int d){
+		data=d;
+		next=null;
 	}
+	}
+
+	public static void main(String[] args) {
+		Linkedlist llist = new Linkedlist();
+		
+		llist.head = new Node(1);
+		Node second = new Node(2);
+		Node third = new Node(3);
+		
+		llist.head.next = second;   //Link first node with second node
+		second.next = third;        //Link second node with third node
+		}
 
 }
